2e8e071b4228d6af513814c0509a4f9c70a9186d
jgh9094 <jgh9094@gmail.com>
2021-04-14 15:07:04 -0400

    added variable for cohort lexicase

--- Experiment/source/config.h
+++ Experiment/source/config.h
@@ -31,6 +31,7 @@ EMP_BUILD_CONFIG(DiaConfig,
   VALUE(NOVEL_K,          size_t,           256,       "Parameter estiamte k-nearest neighbors."),
   VALUE(LEX_EPS,          double,           1.0,       "Parameter estimate for lexicase epsilon."),
   VALUE(DSLEX_PROP,       double,           1.0,       "Parameter for down sampled proportion"),
+  VALUE(COH_LEX_PROP,     double,           1.0,       "Parameter for cohort proportions"),
 
   GROUP(SYSTEMATICS, "Output rates for OpenWorld"),
   VALUE(SNAP_INTERVAL,             size_t,             10000,          "How many updates between prints?"),
