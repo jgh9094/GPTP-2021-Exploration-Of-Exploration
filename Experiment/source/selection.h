16e0ccbe5c3e981421609c83628654261568049f
jgh9094 <jgh9094@gmail.com>
2021-04-14 15:04:21 -0400

    initial implementation of cohort lexicase

--- Experiment/source/selection.h
+++ Experiment/source/selection.h
@@ -238,6 +238,24 @@ class Selection
      */
     size_t DSELexicase(const fmatrix_t & mscore, const double epsi, const ids_t & t_cases);
 
+    /**
+     * Cohort Epsilon Lexicase Selector:
+     *
+     * This function will iterate through a testcase and population cohorts in pairs.
+     * While filtering down the population cohort with its paired paired testcase cohort.
+     * Solutions within an epsilon distance from the optimal will pass through the filter.
+     *
+     * In the event of ties on the last testcase being used, a solution will be selected at random.
+     *
+     * @param mscore Matrix of solution fitnesses (must be the same amount of fitness per solution)(mscore.size => # of orgs).
+     * @param epsi Epsilon threshold value.
+     * @param pop_coh Vector containing population ids.
+     * @param test_coh Vector containing testcase ids.
+     *
+     * @return A single winning solution id.
+     */
+    size_t CELexicase(const fmatrix_t & mscore, const double epsi, const ids_t & pop_coh, const ids_t & test_coh);
+
   private:
 
     // random pointer from world.h
@@ -334,6 +352,9 @@ Selection::fitgp_t Selection::FitnessGroup(const score_t & score)
 
 Selection::cohort_t Selection::CohortGeneration(const size_t N, const double P)
 {
+  // quick checks
+  emp_assert(0 < N); emp_assert(0 < P);
+
   // number of objects in a cohort
   const double proportion = static_cast<double>(N) * P;
   const size_t coh_size = static_cast<size_t>(proportion);
@@ -673,6 +694,67 @@ size_t Selection::DSELexicase(const fmatrix_t & mscore, const double epsi, const
   size_t wid = emp::Choose(*random, filter.size(), 1)[0];
 
   return filter[wid];
+}
+
+size_t Selection::CELexicase(const fmatrix_t & mscore, const double epsi, const ids_t & pop_coh, const ids_t & test_coh)
+{
+  // quick checks
+  emp_assert(0 < mscore.size()); emp_assert(0 <= epsi);
+  emp_assert(0 < pop_coh.size()); emp_assert(0 < test_coh.size());
+
+  // create a vector of shuffled testcase ids
+  ids_t test_id(test_coh.size());
+  std::iota(test_id.begin(), test_id.end(), 0);
+  emp::Shuffle(*random, test_id);
+
+  // create vector to hold filtered elite solutions from pop_coh
+  ids_t filter(pop_coh.size());
+  std::iota(filter.begin(), filter.end(), 0);
+
+  // iterate through testcases until we are out or have a winner
+  size_t tcnt = 0;
+  while(tcnt < test_coh.size() && filter.size() != 1)
+  {
+    // testcase we are randomly evaluating
+    size_t testcase = test_coh[test_id[tcnt]];
+
+    // create vector of current filter solutions
+    score_t scores(filter.size());
+    for(size_t i = 0; i < filter.size(); ++i)
+    {
+      // grab solution from pop_coh
+      const size_t sol = pop_coh[filter[i]];
+
+      // make sure each solutions vector is the right size
+      scores[i] = mscore[sol][testcase];
+    }
+
+    // group org ids by performance in descending order
+    fitgp_t group = FitnessGroup(scores);
+
+    // update the filter vector with pop ids that are worthy
+    ids_t temp_filter = filter;
+    filter.clear();
+    for(const auto & p : group)
+    {
+      if(Distance(group.begin()->first, p.first) <= epsi)
+      {
+        for(auto id : p.second)
+        {
+          filter.push_back(temp_filter[id]);
+        }
+      }
+      else{break;}
+    }
+
+    ++tcnt;
+  }
+
+  // Get a random position from the remaining filtered solutions (may be one left too)
+  emp_assert(0 < filter.size());
+  size_t wid = emp::Choose(*random, filter.size(), 1)[0];
+
+  return pop_coh[filter[wid]];
 }
 
 ///< helper functions
